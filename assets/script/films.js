83e425dbaad27acd6f8e486d5aad45212d9ca99f
clouisegzb <louisiane.cloison@gmail.com>
2024-12-06 11:25:59 +0100

    test recup

--- assets/script/films.js
+++ assets/script/films.js
@@ -18,135 +18,108 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 ;
 // ***********barre de recherche
-document.addEventListener('DOMContentLoaded', () => {
-    const searchIcon = document.querySelector('.search-icon');
-    const searchBox = document.getElementById('search-box');
-    const filterAllButton = document.getElementById('filter-all');
-    const filterAnimationButton = document.getElementById('filter-animation');
-    const filterMoviesButton = document.getElementById('filter-movies');
-    let filterType = 'all'; // Type de filtre par défaut
+// document.addEventListener('DOMContentLoaded', () => {
+//     const searchIcon = document.querySelector('.search-icon');
+//     const searchBox = document.getElementById('search-box');
+//     // const filterAllButton = document.getElementById('filter-all');
+//     // const filterAnimationButton = document.getElementById('filter-animation');
+//     // const filterMoviesButton = document.getElementById('filter-movies');
+//     let filterType = 'all'; // Type de filtre par défaut
 
-    // Bascule la visibilité de la barre de recherche lors du clic sur l'icône
-    searchIcon.addEventListener('click', () => {
-        searchBox.classList.toggle('active'); // Ajoute ou supprime la classe active
-        searchBox.focus(); // Focalise la barre de recherche pour permettre la saisie de texte
-    });
+//     // Bascule la visibilité de la barre de recherche lors du clic sur l'icône
+//     searchIcon.addEventListener('click', () => {
+//         searchBox.classList.toggle('active'); // Ajoute ou supprime la classe active
+//         searchBox.focus(); // Focalise la barre de recherche pour permettre la saisie de texte
+//     });
 
-    // Gère l'événement "Enter" pour déclencher la recherche avec le filtre actuel
-    searchBox.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-            event.preventDefault(); // Empêche le rechargement de la page
-            const searchQuery = searchBox.value.trim(); // Récupère la valeur de la barre de recherche
-            if (searchQuery) {
-                // Redirige vers la page des résultats avec la requête de recherche et le filtre
-                window.location.href = `resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
-            }
-        }
-    });
+//     // Gère l'événement "Enter" pour déclencher la recherche avec le filtre actuel
+//     searchBox.addEventListener('keydown', (event) => {
+//         if (event.key === 'Enter') {
+//             event.preventDefault(); // Empêche le rechargement de la page
+//             const searchQuery = searchBox.value.trim(); // Récupère la valeur de la barre de recherche
+//             if (searchQuery) {
+//                 // Redirige vers la page des résultats avec la requête de recherche et le filtre
+//                 window.location.href = `resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
+//             }
+//         }
+//     });
 
-    document.addEventListener('DOMContentLoaded', () => {
-        const searchIcon = document.querySelector('.search-icon');
-        const searchBox = document.getElementById('search-box');
-        const form = document.getElementById('searchForm');
-        let filterType = 'all'; // Type de filtre par défaut
+//     document.addEventListener('DOMContentLoaded', () => {
+//         const searchIcon = document.querySelector('.search-icon');
+//         const searchBox = document.getElementById('search-box');
+//         const form = document.getElementById('searchForm');
+//         let filterType = 'all'; // Type de filtre par défaut
     
-        // Ajoute un événement au clic sur l'icône de recherche
-        searchIcon.addEventListener('click', () => {
-            searchBox.classList.toggle('active'); // Ajoute ou supprime la classe active
-            searchBox.focus(); // Focalise la barre de recherche pour permettre la saisie de texte
-        });
+//         // Ajoute un événement au clic sur l'icône de recherche
+//         searchIcon.addEventListener('click', () => {
+//             searchBox.classList.toggle('active'); // Ajoute ou supprime la classe active
+//             searchBox.focus(); // Focalise la barre de recherche pour permettre la saisie de texte
+//         });
     
-        // Gestion de l'événement "keydown" sur la barre de recherche
-        searchBox.addEventListener('keydown', (event) => {
-            if (event.key === 'Enter') {
-                event.preventDefault();
-                const searchQuery = searchBox.value.trim();
-                if (searchQuery) {
-                    // Redirige vers la page des résultats avec la requête de recherche et le filtre
-                    window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
-                }
-            }
-        });
+//         // Gestion de l'événement "keydown" sur la barre de recherche
+//         searchBox.addEventListener('keydown', (event) => {
+//             if (event.key === 'Enter') {
+//                 event.preventDefault();
+//                 const searchQuery = searchBox.value.trim();
+//                 if (searchQuery) {
+//                     // Redirige vers la page des résultats avec la requête de recherche et le filtre
+//                     window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
+//                 }
+//             }
+//         });
     
-        // Gestion de la soumission du formulaire
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Empêche le rechargement de la page
-            const searchQuery = searchBox.value.trim();
-            if (searchQuery) {
-                // Redirige vers la page des résultats avec la requête de recherche et le filtre
-                window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
-            }
-        });
-    });
+//         // Gestion de la soumission du formulaire
+//         form.addEventListener('submit', function(event) {
+//             event.preventDefault(); // Empêche le rechargement de la page
+//             const searchQuery = searchBox.value.trim();
+//             if (searchQuery) {
+//                 // Redirige vers la page des résultats avec la requête de recherche et le filtre
+//                 window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
+//             }
+//         });
+//     });
     
 
-    // Événements de clic sur les boutons de filtre
-    filterAllButton.addEventListener('click', () => {
-        applyFilter('all');
-    });
+//     // Événements de clic sur les boutons de filtre
+//     filterAllButton.addEventListener('click', () => {
+//         applyFilter('all');
+//     });
 
-    filterAnimationButton.addEventListener('click', () => {
-        applyFilter('animation');
-    });
+//     filterAnimationButton.addEventListener('click', () => {
+//         applyFilter('animation');
+//     });
 
-    filterMoviesButton.addEventListener('click', () => {
-        applyFilter('movies');
-    });
-});
-
-
-
-// Événements de soumission et de recherche
-    // Gestion des filtres pour la recherche
-    function applyFilter(newFilterType) {
-        filterType = newFilterType;
-        const searchQuery = searchBox.value.trim(); // Récupère la valeur actuelle de la barre de recherche
-        if (searchQuery) {
-            // Redirige vers la page des résultats avec la requête de recherche et le filtre appliqué
-            window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
-        }
-    }
+//     filterMoviesButton.addEventListener('click', () => {
+//         applyFilter('movies');
+//     });
+// });
 
 
-input.addEventListener('keyup', event => {
-    if (event.key === 'Enter') {
-        const searchQuery = input.value.trim();
-        search(searchQuery);
-    }
-});
 
-largeSearchBox.addEventListener('keyup', event => {
-    if (event.key === 'Enter') {
-        const searchQuery = largeSearchBox.value.trim();
-        search(searchQuery);
-    }
-});
+// // Événements de soumission et de recherche
+//     // Gestion des filtres pour la recherche
+//     function applyFilter(newFilterType) {
+//         filterType = newFilterType;
+//         const searchQuery = searchBox.value.trim(); // Récupère la valeur actuelle de la barre de recherche
+//         if (searchQuery) {
+//             // Redirige vers la page des résultats avec la requête de recherche et le filtre appliqué
+//             window.location.href = `/html/resultas-barre-de-recherche.html?query=${encodeURIComponent(searchQuery)}&filter=${filterType}`;
+//         }
+//     }
 
-largeSearchButton.addEventListener('click', () => {
-    const searchQuery = largeSearchBox.value.trim();
-    search(searchQuery);
-});
 
-// Événements de clic sur les boutons de filtre
-filterAllButton.addEventListener('click', () => {
-    filterType = 'all';
-    const searchQuery = largeSearchBox.value.trim() || input.value.trim();
-    search(searchQuery);
-});
+// input.addEventListener('keyup', event => {
+//     if (event.key === 'Enter') {
+//         const searchQuery = input.value.trim();
+//         search(searchQuery);
+//     }
+// });
 
-filterAnimationButton.addEventListener('click', () => {
-    filterType = 'animation';
-    const searchQuery = largeSearchBox.value.trim() || input.value.trim();
-    search(searchQuery);
-});
 
-filterMoviesButton.addEventListener('click', () => {
-    filterType = 'movies';
-    const searchQuery = largeSearchBox.value.trim() || input.value.trim();
-    search(searchQuery);
-});
 
-// Charger les résultats lorsque la page est chargée
+
+
+// // Charger les résultats lorsque la page est chargée
 window.addEventListener('load', loadResults);
 
 
@@ -307,12 +280,12 @@ fetchFilms(currentPageFilm);
 // Filtre barre de recherche avec tmdb
 const form = document.getElementById('searchForm');
 const input = document.getElementById('search-box'); // Petite barre de recherche
-const largeSearchBox = document.getElementById('large-search-box');
+// const largeSearchBox = document.getElementById('large-search-box');
 const results = document.getElementById('results');
 const filterAllButton = document.getElementById('filter-all');
 const filterAnimationButton = document.getElementById('filter-animation');
 const filterMoviesButton = document.getElementById('filter-movies');
-const largeSearchButton = document.getElementById('large-search-button'); // Nouveau bouton de recherche pour la grande barre
+// const largeSearchButton = document.getElementById('large-search-button'); // Nouveau bouton de recherche pour la grande barre
 
 let _imageBaseUrl, _imageSizes;
 let filterType = 'all'; // Type de filtre par défaut
